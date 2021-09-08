d7bf80c954fdb5ff750bc778af38c1b5615b6cd3
Vitaliy Zarubin <zarubin@surfstudio.ru>
2021-09-08 13:57:34 +0300

    Update check resonse

--- src/main/kotlin/com/keygenqt/response/ResponseRetrofit2.kt
+++ src/main/kotlin/com/keygenqt/response/ResponseRetrofit2.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package com.keygenqt.response
 
 import com.keygenqt.response.HTTPResult.*
@@ -22,79 +22,38 @@ import org.json.JSONArray
 import org.json.JSONObject
 import retrofit2.Response
 
-
-/**
- * Response Check with custom handling JSONObject
- *
- * @since 0.0.1
- * @author Vitaliy Zarubin
- */
-fun <T> Response<T>.responseCheckObject(
-    bodyCheckSuccess: (Response<T>) -> Response<T> = { it },
-    bodyCheckError: ((JSONObject) -> String?)? = null,
-): Response<T> {
-    return bodyCheckError?.let {
-        this.responseCheck(bodyCheckSuccess) {
-            when (it) {
-                is JSONObject -> bodyCheckError.invoke(it)
-                else -> null
-            }
-        }
-    } ?: this.responseCheck()
-}
-
-/**
- * Response Check with custom handling JSONArray
- *
- * @since 0.0.1
- * @author Vitaliy Zarubin
- */
-fun <T> Response<T>.responseCheckArray(
-    bodyCheckSuccess: (Response<T>) -> Response<T> = { it },
-    bodyCheckError: ((JSONArray) -> String?)? = null,
-): Response<T> {
-    return bodyCheckError?.let {
-        this.responseCheck(bodyCheckSuccess) {
-            when (it) {
-                is JSONArray -> bodyCheckError.invoke(it)
-                else -> null
-            }
-        }
-    } ?: this.responseCheck()
-}
-
 /**
  * Check status HTTP response retrofit2
  *
- * @since 0.0.1
+ * @since 0.0.2
  * @author Vitaliy Zarubin
  */
 fun <T> Response<T>.responseCheck(
-    bodyCheckSuccess: (Response<T>) -> Response<T> = { it },
-    bodyCheckError: ((Any) -> String?)? = null,
+    bodyCheck: ((code: HTTPResult, body: String) -> Unit)? = null,
 ): Response<T> {
-    return when (code().toHTTPResult()) {
-        is Result200 -> bodyCheckSuccess.invoke(this)
-        is Result400 -> throw Result400()
-        is Result401 -> throw Result401()
-        is Result404 -> throw Result404()
-        is Result500 -> throw Result500()
-        is Result403 -> throw this.parseApiError(bodyCheckError)?.let { Result403(it) } ?: Result403()
-        is ResultUnknown -> throw this.parseApiError(bodyCheckError)?.let { ResultUnknown(code(), it) }
-            ?: ResultUnknown(code())
+    val codeResult = code().toHTTPResult()
+    val body = this.getStringBody()
+    return bodyCheck?.invoke(codeResult, body).let { null } ?: run {
+        when (codeResult) {
+            is Result200 -> this
+            is Result400 -> throw Result400()
+            is Result401 -> throw Result401()
+            is Result404 -> throw Result404()
+            is Result500 -> throw Result500()
+            is Result403 -> throw body.parseApiError()?.let { Result403(it) } ?: Result403()
+            is ResultUnknown -> throw body.parseApiError()?.let { ResultUnknown(code(), it) } ?: ResultUnknown(code())
+        }
     }
 }
 
 /**
- * Parse Api Error select body
+ * Get String from response
  *
  * @since 0.0.1
  * @author Vitaliy Zarubin
  */
-private fun <T> Response<T>.parseApiError(bodyCheckError: ((Any) -> String?)? = null): String? {
-    return this.errorBody()?.string()?.let {
-        it.parseApiError(bodyCheckError)
-    } ?: this.body().toString().parseApiError(bodyCheckError)
+private fun <T> Response<T>.getStringBody(): String {
+    return this.errorBody()?.string() ?: this.body().toString()
 }
 
 /**
@@ -103,34 +62,26 @@ private fun <T> Response<T>.parseApiError(bodyCheckError: ((Any) -> String?)? = 
  * @since 0.0.1
  * @author Vitaliy Zarubin
  */
-private fun String.parseApiError(bodyCheckError: ((Any) -> String?)? = null): String? {
+private fun String.parseApiError(): String? {
     return try {
-        bodyCheckError?.let {
-            if (startsWith("{")) {
-                bodyCheckError.invoke(JSONObject(this))
-            } else {
-                bodyCheckError.invoke(JSONArray(this))
-            }
-        } ?: run {
-            // fix for app
-            val data = if (startsWith("{") && JSONObject(this).has("errors")) {
-                JSONObject(this).getJSONArray("errors").toString()
-            } else {
-                this
-            }
-            // get json object
-            val obj = if (data.startsWith("[")) {
-                val arr = JSONArray(data)
-                if (arr.length() == 0) throw Exception()
-                arr.getJSONObject(0)
-            } else {
-                JSONObject(data)
-            }
-            // get message
-            return if (obj.has("message")) {
-                obj.getString("message")
-            } else throw Exception()
+        // fix for app
+        val data = if (startsWith("{") && JSONObject(this).has("errors")) {
+            JSONObject(this).getJSONArray("errors").toString()
+        } else {
+            this
         }
+        // get json object
+        val obj = if (data.startsWith("[")) {
+            val arr = JSONArray(data)
+            if (arr.length() == 0) throw Exception()
+            arr.getJSONObject(0)
+        } else {
+            JSONObject(data)
+        }
+        // get message
+        return if (obj.has("message")) {
+            obj.getString("message")
+        } else throw Exception()
     } catch (ex: Exception) {
         null
     }
