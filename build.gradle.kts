4f640e4f3c6513a7970e16d99e7234593b2fc2e8
Margarita <margo.himera@yandex.ru>
2021-12-27 21:15:08 +0300

    Add dependencyResolutionManagement

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,13 @@
 buildscript {
 
     val kotlinVersion: String by project
-    val gradleVersion: String by project
 
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:$gradleVersion")
+        classpath("com.android.tools.build:gradle:7.0.4")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
@@ -25,12 +24,5 @@ subprojects {
             target("**/*.kt")
             licenseHeaderFile(file("${project.rootDir}/spotless/LicenseHeader"))
         }
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
     }
 }
